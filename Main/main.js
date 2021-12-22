0986da0626d494741e9d16ffe3628179853b7a87
Juwon Oh <edmaster41@gmail.com>
2021-12-22 14:24:23 -0800

    linking the end

--- Main/main.js
+++ Main/main.js
@@ -76,7 +76,7 @@ var addingEmployee = () => {
                     addingEmployee()
                 } else {
                     //need function for html
-
+                    end()
                 }
             })
         })
