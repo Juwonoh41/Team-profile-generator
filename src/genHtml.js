e08462d2a0aa06b597208637409524831e685c24
Juwonoh41 <edmaster41@gmail.com>
2022-02-10 15:39:36 -0800

    Inter n Engineer

--- src/genHtml.js
+++ src/genHtml.js
@@ -1,4 +1,5 @@
 const getTeam = (team) => {
+
     const getManager = () => {
         return `
         <div class="card employee-card">
@@ -11,6 +12,38 @@ const getTeam = (team) => {
                 <li class="list-group-item">ID: ${manager.gId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${manager.gEmail()}">${manager.getEmail()}</a></li>
                 <li class="list-group-item">Office number: ${manager.gOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>`
+    }
+    const getIntern = () => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.gName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.gId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.gEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.gSchool()}</li>
+            </ul>
+        </div>
+    </div>`
+    }
+    const getEngineer = () => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.gName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.gId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.gEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.gGithub()}</li>
             </ul>
         </div>
     </div>`
