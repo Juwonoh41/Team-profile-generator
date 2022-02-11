b4e9bbff7dfe8697249ea7ed9731ff337041d365
Juwonoh41 <edmaster41@gmail.com>
2022-02-10 17:36:14 -0800

    index.js fin

--- index.js
+++ index.js
@@ -18,8 +18,233 @@ const menu = () => {
                 type: 'input',
                 name: 'managersName',
                 message: "Enter the manager's name",
-                
-            }
+                validate: (answer) => {
+                    if (answer !== '') {
+                      return true;
+                    }
+                    return 'Name must contain a character';
+                  },
+            },
+            {
+                type: 'input',
+                name: 'managersId',
+                message: "Enter the id number",
+                validate: (answer) => {
+                  const passed = answer.match(/^[1-9]\d*$/);
+                  if (passed) {
+                    return true;
+                  }
+                  return 'Number must be above 0';
+                }, 
+            },
+            {
+                type: 'input',
+                name: 'managersEmail',
+                message: "Enter the email",
+                validate: (answer) => {
+                  const passed = answer.match(/\S+@\S+\.\S+/);
+                  if (passed) {
+                    return true;
+                  }
+                  return 'Enter a valid email.';
+                },
+              },
+              {
+                type: 'input',
+                name: 'managersOfficeNumber',
+                message: "Enter the office number",
+                validate: (answer) => {
+                  const passed = answer.match(/^[1-9]\d*$/);
+                  if (passed) {
+                    return true;
+                  }
+                  return 'Number must be aboved zero';
+                },
+              },    
+            
         ])
+        .then((answers) => {
+            const manager = new Manager(
+              answers.managerName,
+              answers.managerId,
+              answers.managerEmail,
+              answers.managerOfficeNumber
+            );
+            members.push(manager);
+            id.push(answers.managerId);
+            team();
+          });
     }
+    //adding team member card
+function team(){
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'memberRole',
+          message: 'Click on the role you wish to add.',
+          choices: [
+            'Engineer',
+            'Intern',
+            "I'm done.",
+          ],
+        },
+      ])
+      .then((userChoice) => {
+        switch (userChoice.memberChoice) {
+          case 'Engineer':
+            engineer();
+            break;
+          case 'Intern':
+            intern();
+            break;
+          default:
+            done();
+        }
+      });
 }
+
+function engineer(){
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'eName',
+          message: "Enter the name",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Must enter at least one character';
+          },
+        },
+        {
+            type: 'input',
+            name: 'eId',
+            message: "Enter the id",
+            validate: (answer) => {
+              const passed = answer.match(/^[1-9]\d*$/);
+              if (passed) {
+                if (id.includes(answer)) {
+                  return 'Enter different number.';
+                } else {
+                  return true;
+                }
+              }
+              return 'Enter a number higher then zero.';
+            },
+          },
+          {
+            type: 'input',
+            name: 'eEmail',
+            message: "Enter the email.",
+            validate: (answer) => {
+              const passed = answer.match(/\S+@\S+\.\S+/);
+              if (passed) {
+                return true;
+              }
+              return 'Enter a valid email';
+            },
+          },
+          {
+            type: 'input',
+            name: 'eGithub',
+            message: "Enter the github username",
+            validate: (answer) => {
+              if (answer !== '') {
+                return true;
+              }
+              return 'Must have at least one character';
+            },
+          },
+        ])
+        .then((answer) => {
+            const engineer = new Engineer(
+              answer.eName,
+              answer.eId,
+              answer.eEmail,
+              answer.eGithub
+            );
+            members.push(engineer);
+            id.push(answers.engineerId);
+            team();
+          });
+}
+function intern(){
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'iName',
+          message: "Enter the name",
+          validate: (answer) => {
+            if (answer !== '') {
+              return true;
+            }
+            return 'Must contain at least one character';
+          },
+        },
+        {
+            type: 'input',
+            name: 'iId',
+            message: "Enter the id",
+            validate: (answer) => {
+              const passed = answer.match(/^[1-9]\d*$/);
+              if (passed) {
+                if (id.includes(answer)) {
+                  return 'Enter different number';
+                } else {
+                  return true;
+                }
+              }
+              return 'Number must be greater then zero.';
+            },
+          },
+          {
+            type: 'input',
+            name: 'iEmail',
+            message: "Enter the email",
+            validate: (answer) => {
+              const passed = answer.match(/\S+@\S+\.\S+/);
+              if (passed) {
+                return true;
+              }
+              return 'Email is invalid';
+            },
+          },
+          {
+            type: 'input',
+            name: 'iSchool',
+            message: "Enter the school",
+            validate: (answer) => {
+              if (answer !== '') {
+                return true;
+              }
+              return 'Must contain at least one character';
+            },
+          },
+        ])
+        .then((answers) => {
+            const intern = new Intern(
+              answers.iName,
+              answers.iId,
+              answers.iEmail,
+              answers.iSchool
+            );
+            members.push(intern);
+            id.push(answers.internId);
+            team();
+          });
+
+}
+function done(){
+    if (!fs.existsSync(DIST_DIR)) {
+        fs.mkdirSync(DIST_DIR);
+      }
+      fs.writeFileSync(distPath, render(teamMembers), 'utf-8');
+    }
+  
+    manager();
+}
+
+menu()
--- src/genHtml.js
+++ src/genHtml.js
@@ -6,14 +6,14 @@ const getTeam = (team) => {
         return `
         <div class="card employee-card">
         <div class="card-header">
-            <h2 class="card-title">${manager.gName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role()}</h3>
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.gId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.gEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.gOfficeNumber()}</li>
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
             </ul>
         </div>
     </div>`
@@ -22,14 +22,14 @@ const getTeam = (team) => {
         return `
         <div class="card employee-card">
         <div class="card-header">
-            <h2 class="card-title">${manager.gName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role()}</h3>
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.gId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.gEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.gSchool()}</li>
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getSchool()}</li>
             </ul>
         </div>
     </div>`
@@ -38,14 +38,14 @@ const getTeam = (team) => {
         return `
         <div class="card employee-card">
         <div class="card-header">
-            <h2 class="card-title">${manager.gName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.role()}</h3>
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.gId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.gEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.gGithub()}</li>
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getGithub()}</li>
             </ul>
         </div>
     </div>`
